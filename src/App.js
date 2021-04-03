f1e3903d62ace0a4f9aed1f9b671db3ac04e8f40
Kyle Z Condie <barber.von.frankenshtien@gmail.com>
2021-04-03 15:58:31 -0600

    working on aboutme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 //import PortfolioPage from "./components/PortfolioPage";
 import NavHeader from "./components/NavHeader";
+import AboutMe from "./components/AboutMe";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <NavHeader />;
+  return (
+    <main>
+      <NavHeader></NavHeader>
+      <AboutMe></AboutMe>
+    </main>
+  );
 }
 
 export default App;
--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 
 function NavHeader(props) {
-  const tabs = ["Home", "About", "Blog", "Contact"];
+  const tabs = ["About Me", "Portfolio", "Contact", "Resume"];
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar sticky="top" bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="#">Kyle Condie</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
@@ -21,29 +21,6 @@ function NavHeader(props) {
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-
-    // <nav className="navbar navbar-dark bg-dark">
-    //   <a className="navbar-brand" href="#">
-    //     Kyle Condie
-    //   </a>
-    //   <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    //     <ul className="navbar-nav mr-auto">
-    // {tabs.map((tab) => (
-    //   <li key={tab}>
-    //     <a
-    //       href={"#" + tab.toLowerCase()}
-    //       onClick={() => props.handlePageChange(tab)}
-    //       className={
-    //         props.currentPage === tab ? "nav-link active" : "nav-link"
-    //       }
-    //           >
-    //             {tab}
-    //           </a>
-    //         </li>
-    //       ))}
-    //     </ul>
-    //   </div>
-    // </nav>
   );
 }
 
